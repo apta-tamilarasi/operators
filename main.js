13d4005c066164f1a1f9549193b2f262b5d704e1
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-11-26 07:43:29 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,111 @@
+var a=10, b=5, c=10, d="apta", e="tamil", f="durga", g="10", h="apta";
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=10, h=apta");
+console.log("1) ADDITION");
+console.log("a+b="+(a+b));
+console.log("d+e="+(a+d));
+console.log("a+d="+(a+d));
+console.log("a-e="+(a-e));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil");
+console.log("2) SUBTRACTION");
+console.log("a-b="+(a-b));
+console.log("d-e="+(d-e));
+console.log("a-d="+(a-d));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil");
+console.log("3) MULTIPLY");
+console.log("a*b="+(a*b));
+console.log("d*e="+(d*e));
+console.log("a*d="+(a*d));
+
+console.log("a=10, b=4, c=10, d=apta, e=tamil");
+console.log("4) DIVISION");
+console.log("a/b="+(a/b));
+console.log("d/e="+(d/e));
+console.log("a/d="+(a/d));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil");
+console.log("5) MODULO");
+console.log("a%b="+(a%b));
+console.log("d%e="+(d%e));
+console.log("a%d="+(a%d));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("6) GREATER THAN");
+console.log("a>b="+(a>b));
+console.log("d>e="+(d>e));
+console.log("a>d="+(a>d));
+console.log("c>e="+(b>e));
+console.log("e>d="+(e>d));
+console.log("f>g="+(f>g));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil");
+console.log("7) LESS THAN");
+console.log("a<b="+(a<b));
+console.log("d<e="+(d<e));
+console.log("a<d="+(a<d));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil");
+console.log("8) EXPONENTIAL");
+console.log("a**b="+(a**b));
+console.log("d**e="+(d**e));
+console.log("a**d="+(a**d));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("9) == EQUAL");
+console.log("a==b="+(a==b));
+console.log("d==e="+(d==e));
+console.log("a==d="+(a==d));
+console.log("b==e="+(b==e));
+console.log("e==f="+(e==f));
+console.log("g==c="+(g==c));
+console.log("d==h="+(d==h));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("10) === DATATYPE EQUAL ");
+console.log("a===c="+(a===c));
+console.log("d===e="+(d===e));
+console.log("c===g="+(c===g));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("11) != NOT EQUAL");
+console.log("a!=b="+(a!=b));
+console.log("a!=c="+(a!=c));
+console.log("d!=e="+(d!=e));
+console.log("a!=d="+(a!=d));
+console.log("f!=e="+(f!=e));
+console.log("d!=h="+(d!=h));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("12) !== DATA NOT EQUAL");
+console.log("a!==b="+(a!==b));
+console.log("d!==e="+(d!==e));
+console.log("a!==d="+(a!==d));
+console.log("a!==c="+(a!==c));
+console.log("d!==h="+(d!==h));
+console.log("c!==g="+(c!==g));
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("13) AND && ");
+console.log("a&&b="+(a&&b));
+console.log("d&&e="+(d&&e));
+console.log("a&&d="+(a&&d));
+console.log("(a>b)&&(d+e)="+((a>b)&&(d+e)));
+console.log("(a<b)&&(d+e)="+((a<b)&&(d+e)));
+
+
+console.log("a=10, b=5, c=10, d=apta, e=tamil, f=durga, g=string10, h=apta");
+console.log("13) OR || ");
+console.log("a||b="+(a||b));
+console.log("d||e="+(d||e));
+console.log("a||d="+(a||d));
+console.log("(a+b)||(d+e)="+((a+b)||(d+e)));
+console.log("(a>b)||(d+e)="+((a>b)||(d+e)));
+console.log("(a<b)||(d+e)="+((a<b)||(d+e)));
+console.log("(a<b)||(d+e)||(a>b)="+((a<b)||(d+e)||(a>b)));
+console.log("(a<b)||(d+e)||(a>b)||(e+f)="+((a<b)||(d+e)||(a>b)||(e+f)));
+console.log("(a>b)||(d+e)||(a<b)||(e+f)="+((a>b)||(d+e)||(a<b)||(e+f)));
+
+
+
+
